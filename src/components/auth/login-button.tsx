a3c0094fd6a633b56d0cfc4187a6920b17a8cb5d
RaamPrashathT <131695872+RaamPrashathT@users.noreply.github.com>
2025-05-25 23:36:49 +0530

    structure base

---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+interface LoginButtonProps {
+    children: React.ReactNode;
+    mode?: "modal" | "redirect",
+    asChild?: boolean;
+}
+
+export const LoginButton = ({
+    children,
+    mode,
+    asChild
+}: LoginButtonProps) => {
+
+    const router = useRouter();
+    const onClick = () => {
+        router.push("auth/login");
+    }
+
+    if(mode === "modal") {
+        return(
+            <div>
+                TODO: Implement modal
+            </div>
+        )
+    }
+
+    return(
+        <div onClick={onClick} className="cursor-pointer">
+            {children}
+        </div>
+    )
+}
